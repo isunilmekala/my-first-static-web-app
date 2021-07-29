22a1ca0b3cf54e463a5c051768065db5953a5c13
Sunil Mekala <smekala@innominds.com>
2021-07-29 13:23:04 +0530

    Added API using azure function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{ value }}</div>`,
 })
 export class AppComponent {
   value = 'World';
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('/api/message')
+      .subscribe((resp: any) => (this.value = resp.body));
+  }
 }
